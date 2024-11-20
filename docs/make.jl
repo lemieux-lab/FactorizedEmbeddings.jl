8805b18346bd02504640d338d5f4d58b2592e90d
sauves <leonardsauve@gmail.com>
2024-11-20 15:34:38 -0500

    added make

---
+++ docs/make.jl
@@ -0,0 +1,12 @@
+push!(LOAD_PATH,"../src/")
+using FactorizedEmbeddings
+using Documenter
+makedocs(
+         sitename = "FactorizedEmbeddings.jl",
+         modules  = [FactorizedEmbeddings],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/lemieux-lab/FactorizedEmbeddings.jl",
+)
