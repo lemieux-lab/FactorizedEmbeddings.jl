37ebf66f482c41e675ec54178def0b110130eec8
sauves <leonardsauve@gmail.com>
2024-11-20 23:33:04 -0500

    tried updating

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,15 @@ using Documenter
 makedocs(
          sitename = "FactorizedEmbeddings.jl",
          modules  = [FactorizedEmbeddings],
+         repo="github.com/lemieux-lab/FactorizedEmbeddings.jl",
          pages=[
                 "Home" => "index.md"
-               ])
-# deploydocs(;
-#     repo="github.com/lemieux-lab/FactorizedEmbeddings.jl",
-#     # remotes="https://github.com/lemieux-lab/FactorizedEmbeddings.jl.git"
-# )
+               ],
+         format=Documenter.HTML(;
+              prettyurls = get(ENV, "CI", "false") == "true"),
+              # assets=["assets/style.css"]
+         )
+deploydocs(;
+    repo="github.com/lemieux-lab/FactorizedEmbeddings.jl",
+    # remotes="https://github.com/lemieux-lab/FactorizedEmbeddings.jl.git"
+)
